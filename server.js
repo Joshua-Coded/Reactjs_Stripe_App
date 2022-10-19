bc6d5044e0bec0f3c97ed272b50d4eca5bca190f
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-10-19 01:17:10 +0100

    added the react functionality

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+var cors = require('cors');
+const stripe = require('stripe')('sk_test_51LuOmmGuFczBDLSuECamD8Au9u2cwBfNo82GrnpZxXcWLBYyulzffpCR5lLX6zFOtpd95ClFS3xde5W4iYNgEQlk00HrMkuQD8');
+
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+    /*
+    req.body.items
+    [
+        {
+            id: 1,
+            quantity: 3
+        }
+    ]
+    stripe wants
+    [
+        {
+            price: 1,
+            quantity: 3
+        }
+    ]
+    */
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+});
+
+app.listen(4000, () => console.log("Listening on port 4000!"));
--- store/src/productsStore.js
+++ store/src/productsStore.js
@@ -1,20 +1,26 @@
-const productsArray = [
+ const productsArray = [
     {
-        id: "1",
-        title: "coffee",
-        price: 4.66,
+        id: "price_1LuOyzGuFczBDLSuyd2HT360",
+        title: "Sneakers",
+        price: 20.0,
     },
 
     {
-        id: "2",
-        title: "sunglasses",
-        price: 4.99,
+        id: "price_1LuP2OGuFczBDLSuMuhwhSIL",
+        title: "Sunglasses",
+        price: 12.0,
     },
     {
-        id: "3",
-        title: "rice",
-        price: 5.00,
+        id: "price_1LuP0oGuFczBDLSukftYY8Af",
+        title: "Headset",
+        price: 15.0,
     },
+    {
+        id: "price_1LuP3qGuFczBDLSuxmG7VRAE",
+        title: "Camera",
+        price: 40.0,
+    },
+    
 ]
 
 function getProductData(id) {
