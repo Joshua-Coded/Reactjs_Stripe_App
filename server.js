fc29b970d932bfa295b7da91a93d7b6b9f4f36da
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-10-19 03:23:13 +0100

    files

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
+
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/checkout', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
+
+
 app.post("/checkout", async (req, res) => {
     /*
     req.body.items
